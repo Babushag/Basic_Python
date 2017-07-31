200000094701ca14ea25cdb64ebda3484eed7217
Babushag <30575703+Babushag@users.noreply.github.com>
2017-07-31 17:05:13 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,44 +1,21 @@
-class Node:
-  
-    
-def __init__(self, cargo=None, next=None):
+class Node:   
+  def __init__(self, cargo=None, next=None):
+  self.cargo = cargo
+  self.next  = next
         
-self.cargo = cargo
+  def __str__(self):
+  return str(self.cargo)
         
-self.next  = next
+   def print_list(self,node):
+   while node is not None:
+   print(node, end=" ")
+   node = node.next
+   print()
         
-        
-    
-
-def __str__(self):
-        
-return str(self.cargo)
-        
-        
-    
-
-def print_list(node):
-    
-while node is not None:
-        
-print(node, end=" ")
-        
-node = node.next
-    
-print()
-        
- 
-
 node1 = Node(1)
- 
 node2 = Node(2)
- 
 node3 = Node(3)
- 
 node1.next = node2
- 
 node2.next = node3
- 
- 
-print_list(node1)
+node1.print_list(node1)
     
