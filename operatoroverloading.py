76a7f10cf405cffe4f8ec4ff62911643a376b322
Babushag <30575703+Babushag@users.noreply.github.com>
2017-07-31 13:22:42 +0530

    Add files via upload

---
+++ operatoroverloading.py
@@ -0,0 +1,15 @@
+#here is the concept of operator overloading in python"+"
+
+var1=3
+
+var2=5
+
+var3="hi "
+
+var4="welcome"
+
+
+
+print("output for strings for + operator is cocatinating as shown",var3+var4)
+
+print("output for integers for +operator is adding integers as shown",var1+var2)
