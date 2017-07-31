b4e5ecb4370dfc830f48c64422a3c949b314b199
Babushag <30575703+Babushag@users.noreply.github.com>
2017-07-31 12:50:01 +0530

    Add files via upload

---
+++ Encapsulation_using_classes.py
@@ -0,0 +1,41 @@
+# example of encapsulation using classes and object
+ 
+class Worker:
+   
+#'Common base class for all workers'
+   
+Count = 0
+
+   
+
+def __init__(self, name, salary):
+      
+self.name = name
+     
+self.salary = salary
+     
+ Worker.Count += 1
+   
+ 
+  
+def displayCount(self):
+     
+print "Total Worker %d" % Worker.Count
+
+ 
+ 
+def displayWorker(self):
+      
+print "Name : ", self.name,  ", Salary: ", self.salary
+
+
+
+w1 = Worker("Ram", 2000)
+
+w2 = Worker("Shaam", 5000)
+
+w1.displayWorker()
+
+w2.displayWorker()
+
+print "Total Employee %d" % Worker.Count
